195d453dcf7d20c9b393ee195c155e23df64e4da
Martin Kamenov <martykam@abv.bg>
2017-08-15 18:57:56 +0300

    Added players to home.

--- soccerProject/app.js
+++ soccerProject/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const pug = require('pug');
 const homeRouter = require('./static/routes/home-router');
+const path = require('path');
 
 const app = express();
 app.set('view engine', 'pug');
+app.set('views', __dirname + '\\views');
+app.use('/public', express.static(path.join(__dirname, './public')));
+app.use('/libs', express.static(path.join(__dirname, '../node_modules')));
 
 homeRouter(app);
 
 app.listen(3001, () => {
     console.log('Magic is happening at 3001');
-})
+});
--- soccerProject/static/routes/home-router.js
+++ soccerProject/static/routes/home-router.js
@@ -1,10 +1,21 @@
 const { Router } = require('express');
+const http = require('http');
+const request = require("request");
 
 const attach = function(app) {
     const router = new Router();
     router
         .get('/', (req, res) => {
-            res.render('home');
+            request({
+                url: 'http://www.easports.com/fifa/ultimate-team/api/fut/item?page=1',
+                json: true
+            }, function(error, response, body) {
+
+                if (!error && response.statusCode === 200) {
+                    const players = body.items;
+                    res.status(202).render('players', { players });
+                }
+            });
         });
     app.use('/', router);
 };
