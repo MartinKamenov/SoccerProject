bbaf707267f0576ad8000be2f52913b985060f5f
Martin Kamenov <martykam@abv.bg>
2017-08-16 15:17:58 +0300

    Added many players, and option to check the player

--- soccerProject/app.js
+++ soccerProject/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const pug = require('pug');
 const homeRouter = require('./static/routes/home-router');
 const path = require('path');
+const Database = require('./database/mongodb');
+//const connectionString = 'mongodb://mk10:onetwotwo@ds145193.mlab.com:45193/footballproject';
+const connectionString = 'mongodb://localhost/footballproject';
+
+const database = new Database(connectionString);
 
 const app = express();
 app.set('view engine', 'pug');
@@ -9,7 +14,7 @@ app.set('views', __dirname + '\\views');
 app.use('/public', express.static(path.join(__dirname, './public')));
 app.use('/libs', express.static(path.join(__dirname, '../node_modules')));
 
-homeRouter(app);
+homeRouter(app, database);
 
 app.listen(3001, () => {
     console.log('Magic is happening at 3001');
--- soccerProject/static/routes/home-router.js
+++ soccerProject/static/routes/home-router.js
@@ -2,18 +2,37 @@ const { Router } = require('express');
 const http = require('http');
 const request = require("request");
 
-const attach = function(app) {
+const attach = function(app, database) {
     const router = new Router();
     router
         .get('/', (req, res) => {
+            const page = req.query.page;
             request({
-                url: 'http://www.easports.com/fifa/ultimate-team/api/fut/item?page=1',
+                url: 'http://www.easports.com/fifa/ultimate-team/api/fut/item?page=' + page + '&quality=bronze%2Crare_bronze%2Csilver%2Crare_silver%2Cgold%2Crare_gold',
                 json: true
-            }, function(error, response, body) {
-
+            }, async function(error, response, body) {
                 if (!error && response.statusCode === 200) {
                     const players = body.items;
-                    res.status(202).render('players', { players });
+                    res.status(200).render('players', { players });
+                }
+            });
+        })
+        .get('/:country/:id', (req, res) => {
+            const name = req.params.name;
+            const country = req.params.country;
+            const id = req.params.id;
+            request({
+                url: 'http://www.easports.com/fifa/ultimate-team/api/fut/item?country=' + country +
+                    '&quality=bronze%2Crare_bronze%2Csilver%2Crare_silver%2Cgold%2Crare_gold',
+                json: true
+            }, async function(error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    const players = body.items;
+                    const player = players.find((p) => p.id == id);
+                    if (player) {
+                        console.log(player);
+                        res.status(200).render('player', { player });
+                    }
                 }
             });
         });
