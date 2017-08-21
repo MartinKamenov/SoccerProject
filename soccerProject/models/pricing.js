15b516e6d756563aea8d0283812ae5a54c9c0098
Martin Kamenov <martykam@abv.bg>
2017-08-21 22:20:16 +0300

    Added pricing for players over 84 overall.

--- soccerProject/models/pricing.js
+++ soccerProject/models/pricing.js
@@ -2,8 +2,20 @@ class Pricer {
     choosePrice(rating, position, age) {
         var price = 0;
         if (rating > 84) {
-            if (position == 'ST' || position == 'CF' || position == 'LF' || position == 'RF' || position == 'LW' || position == 'RW') {
-                price = (rating - 85) * 10000000 + 50000000;
+            if (position == 'ST' || position == 'CF' || position == 'LF' ||
+                position == 'RF' || position == 'LW' || position == 'RW') {
+                price = (rating - 85) * 10000000 + 60000000;
+            }
+            if (position == 'GK') {
+                price = (rating - 85) * 5000000 + 30000000;
+            }
+            if (position == 'CAM' || position == 'RM' || position == 'LM' ||
+                position == 'CM' || position == 'CDM') {
+                price = (rating - 85) * 7000000 + 50000000;
+            }
+            if (position == 'LB' || position == 'RB' || position == 'CB' ||
+                position == 'LWB' || position == 'RWB') {
+                price = (rating - 85) * 6000000 + 40000000;
             }
         }
         return price;
